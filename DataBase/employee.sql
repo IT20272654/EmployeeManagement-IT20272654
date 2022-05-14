2727bea0f4fd710c419d620776b9ddcb3ec1ce20
IT20272654 <shavindurajapaksha@gmail.com>
2022-05-15 01:59:05 +0530

    MySQL DataBase sql file

---
+++ DataBase/employee.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM employee.employee
+LIMIT 0, 1000
+
+-- Date: 2022-05-15 01:54
+*/
+INSERT INTO `` (`employeeID`,`employeeCode`,`employeeName`,`employeePhone`,`employeeDesc`) VALUES (5,'102k','Kamal Silva','07732321231','Admin');
+INSERT INTO `` (`employeeID`,`employeeCode`,`employeeName`,`employeePhone`,`employeeDesc`) VALUES (7,'108r','Rani Perera','0773243238','Admin');
+INSERT INTO `` (`employeeID`,`employeeCode`,`employeeName`,`employeePhone`,`employeeDesc`) VALUES (8,'109k','Nilu Raja','0773243231','Admin');
